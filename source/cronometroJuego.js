1a88433d02c13a6d349330998dc978d3f618b9e9
JuanSolerPardo <165084324+JuanSolerPardo@users.noreply.github.com>
2024-04-05 02:32:50 +0200

    Update cronometroJuego.js

--- source/cronometroJuego.js
+++ source/cronometroJuego.js
@@ -1,1 +1,63 @@
+export default class cronometroJuego
+{
 
+  /** @param {Phaser.Graphics} circle */
+  esfera
+
+  /** @param {number} xPosition
+  xPosition
+
+  /** @param {number} yPosition
+  yPosition
+
+  /** @param {number} radius
+  radius
+
+  /** @param {number} lineThick
+  lineThick
+
+  /** @param {number} lineColor
+  lineColor
+    
+	/** 
+	 * @param {Phaser.Scene} scene 	
+	 */
+	constructor(scene)
+	{		 
+    this.graphics = this.add.graphics();    
+		this.scene = scene	
+    this.xPosition = 400 
+    this.yPosition = 300
+    this.radius = 260
+    this.lineColor = 0xffffff
+    this.lineThick = 0
+    
+    //  With minimal arguments it creates a circle of radius 260 centered on 400x300:
+    this.esfera = new Phaser.Curves.Ellipse(this.xPosition, this.yPosition, this.radius);
+
+	}
+
+	/**
+	 * @param {() => void} callback
+	 * @param {number} duration 
+	 */
+	start()
+	{
+		this.stop()
+	}
+
+	stop()
+	{
+		
+	}
+
+	update()
+	{
+        this.graphics.clear();
+
+        this.graphics.lineStyle(2, this.lineColor, 1);
+
+        this.esfera.draw(this.graphics, 64);
+
+	}
+}
