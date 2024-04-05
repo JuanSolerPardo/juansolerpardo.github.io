ac5794199fed44a26a085ec6c22d10485f8f724b
JuanSolerPardo <165084324+JuanSolerPardo@users.noreply.github.com>
2024-04-05 13:32:05 +0200

    Update faceController.js

--- source/faceController.js
+++ source/faceController.js
@@ -19,40 +19,41 @@ export default class faceController
 	/** 
 	* @param {Phaser.Scene} scene 	
 	*/
-	constructor(scene, xPosition, yPosition, face)
+	constructor(scene, position, face)
 	{		 
 		this.scene = scene	
 		this.crono = new CountDownController(scene);		  	        
-		this.xPosition = xPosition
-		this.yPosition = yPosition
+		this.position = position		
 		this.face = face
+    		
 	
 	}
 	
 	
 	start()
 	{	
-		this.crono.start(this.handleCountDownFinished.bind(this)); 
-		
+		this.crono.start(this.handleCountDownFinished.bind(this),Phaser.Math.RND.between(500,1500)); 
+		this.position.setTexture(this.face); 
 	}
 	
 	update()
 	{		 
 	    this.crono.update();
-	    
+	    if(this.crono.secondsRemain <= 0)
+	    {
+		this.stop()
+	    }
 	}
 	
 	stop()
-	{
-	  	
-	  this.destroy();
-	  
+	{	  	
+	  this.destroy();	  
 	}
 	
 	
 	handleCountDownFinished()
 	{
-		this.stop();
+	   this.stop();
 	}
 
 	
