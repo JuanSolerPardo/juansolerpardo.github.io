ed212339479ca5d1b8ac9dcf135bd9965d151a86
JuanSolerPardo <165084324+JuanSolerPardo@users.noreply.github.com>
2024-04-05 02:44:18 +0200

    Update cronometroJuego.js

--- source/cronometroJuego.js
+++ source/cronometroJuego.js
@@ -1,63 +1,64 @@
 export default class cronometroJuego
 {
 
-  /** @param {Phaser.Graphics} circle */
-  esfera
+/** @param {Phaser.Graphics} circle */
+esfera
+/** @type {Phaser.Scene} */
+scene
 
-  /** @param {number} xPosition
-  xPosition
+/** @param {number} xPosition */
+xPosition
 
-  /** @param {number} yPosition
-  yPosition
+/** @param {number} yPosition */
+yPosition
 
-  /** @param {number} radius
-  radius
+/** @param {number} radius */
+radius
 
-  /** @param {number} lineThick
-  lineThick
+/** @param {number} lineThick */
+lineThick
 
-  /** @param {number} lineColor
-  lineColor
-    
-	/** 
-	 * @param {Phaser.Scene} scene 	
-	 */
-	constructor(scene)
-	{		 
-    this.scene = scene	
-    this.graphics = this.scene.add.graphics();        
-    this.xPosition = 400 
-    this.yPosition = 300
-    this.radius = 260
-    this.lineColor = 0xffffff
-    this.lineThick = 0
-    
-    //  With minimal arguments it creates a circle of radius 260 centered on 400x300:
-    this.esfera = new Phaser.Curves.Ellipse(this.xPosition, this.yPosition, this.radius);
+/** @param {number} lineColor */
+lineColor
 
-	}
+/** 
+* @param {Phaser.Scene} scene 	
+*/
+constructor(scene)
+{		 
+	this.scene = scene	
+	this.graphics = this.scene.add.graphics();        
+	this.xPosition = 400 
+	this.yPosition = 300
+	this.radius = 260
+	this.lineColor = 0xffffff
+	this.lineThick = 0
+	
+	//  With minimal arguments it creates a circle of radius 260 centered on 400x300:
+	this.esfera = new Phaser.Curves.Ellipse(this.xPosition, this.yPosition, this.radius);
 
-	/**
-	 * @param {() => void} callback
-	 * @param {number} duration 
-	 */
-	start()
-	{
-		this.stop()
-	}
+}
 
-	stop()
-	{
-		
-	}
+/**
+* @param {() => void} callback
+* @param {number} duration 
+*/
+start()
+{
+	this.stop()
+}
 
-	update()
-	{
-        this.graphics.clear();
+stop()
+{
 
-        this.graphics.lineStyle(2, this.lineColor, 1);
+}
 
-        this.esfera.draw(this.graphics, 64);
-
-	}
+update()
+{
+	this.graphics.clear();
+	
+	this.graphics.lineStyle(2, this.lineColor, 1);
+	
+	this.esfera.draw(this.graphics, 64);
+}
 }
