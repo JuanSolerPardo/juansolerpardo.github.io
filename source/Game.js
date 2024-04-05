eee35f2c9162085ef5516957d4bd75e897601dfa
JuanSolerPardo <165084324+JuanSolerPardo@users.noreply.github.com>
2024-04-05 22:05:58 +0200

    Update Game.js

--- source/Game.js
+++ source/Game.js
@@ -29,10 +29,10 @@ export class Game extends Phaser.Scene
         { x: 524, y: 695 },
         { x: 734, y: 695 }*/
         this.positions = [   
-         { x: 120, y: 335 },    
-        { x: 316, y: 335 },
+         { x: 125, y: 335 },    
+        { x: 320, y: 335 },
         { x: 515, y: 335 },
-        { x: 705, y: 335 }
+        { x: 700, y: 335 }
         // ... (agrega las demás posiciones aquí)
         ];   
         this.imagesToDisplay = ['img0', 'img1', 'img2', 'img3', 'img4', 'img5', 'img6'];
