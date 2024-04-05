082d898fa6f63ad8e2ea3f793694b2e42f2fb6a3
JuanSolerPardo <165084324+JuanSolerPardo@users.noreply.github.com>
2024-04-05 16:10:38 +0200

    Update faceController.js

--- source/faceController.js
+++ source/faceController.js
@@ -32,8 +32,11 @@ export default class faceController
 	
 	start()
 	{	
-		this.crono.start(this.handleCountDownFinished.bind(this),Phaser.Math.RND.between(500,1500)); 			  
+		this.crono.start(this.handleCountDownFinished.bind(this),Phaser.Math.RND.between(850,1500)); 			  
 		this.position = this.scene.add.image(this.position.x, this.position.y -10, this.face).setOrigin(0.5);
+		this.position.setDisplaySize(30, 30);
+      		this.position.setInteractive();
+      		this.position.on('pointerdown', this.update());
 	}
 	
 	update()
@@ -47,7 +50,7 @@ export default class faceController
 	
 	stop()
 	{	  	
-	  this.destroy();	  
+	  this.destroy(true);	  
 	}
 	
 	
