e5c1dc13b405cbd784f7e08b22030da009165eaa
JuanSolerPardo <165084324+JuanSolerPardo@users.noreply.github.com>
2024-04-05 08:12:57 +0200

    Update faceController.js

--- source/faceController.js
+++ source/faceController.js
@@ -1,1 +1,47 @@
-
+export default class faceController
+{
+	
+	 
+	/** @type {Phaser.Scene} */
+	scene
+		
+	/** @type {Phaser.EventTime}*/
+  crono
+		
+	/** @param {number} xPosition */
+	xPosition
+	
+	/** @param {number} yPosition */
+	yPosition
+	
+	/** 
+	* @param {Phaser.Scene} scene 	
+	*/
+	constructor(scene)
+	{		 
+		this.scene = scene	
+	  this.crono = new   
+		this.xPosition = 540 
+		this.yPosition = 75
+	
+	}
+	
+	
+	start()
+	{	
+		 
+		
+	}
+	
+	stop()
+	{
+	  return;
+	}
+	
+	update()
+	{		 
+		
+	}
+	
+	
+}
